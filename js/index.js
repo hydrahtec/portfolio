5c0dd880359250c9a3b69815ff78297ced8186b1
Eliel S <hydrahtec@gmail.com>
2023-12-17 22:42:36 -0300

    update
    17122023 2242

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
+//formulario
 const form = document.querySelector(".contato");
 
-const mascara = document.querySelector(".mascara");
+const mascara = document.querySelector(".mascara_form");
 
 function open_form () {
     form.style.right = "300px";
@@ -11,3 +12,25 @@ function close_form () {
     form.style.right = "-500px";
     mascara.style.visibility = "hidden";
 }
+
+// toggle mobile
+
+const hamburger = document.querySelector(".btn_hamburger");
+
+const link = document.querySelector(".div_link")
+
+const nav = document.querySelector(".nav");
+
+const links = document.querySelector(".link");
+
+const a = document.querySelectorAll(".a")
+
+hamburger.addEventListener("click", add_class);
+
+function add_class () {
+    hamburger.classList.toggle("open");
+    link.classList.toggle("active");
+};
+
+links.addEventListener("click", add_class);
+a.addEventListener("click", add_class);
